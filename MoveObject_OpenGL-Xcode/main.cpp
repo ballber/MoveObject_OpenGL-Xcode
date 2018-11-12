c4a438c83bcc5fe5eb5227d0d31fa3833ab826ed
Josep <josep@mbp13-2.local>
2018-11-13 00:50:31 +0100

    Initial Commit

---
+++ MoveObject_OpenGL-Xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MoveObject_OpenGL-Xcode
+//
+//  Created by Josep on 13/11/18.
+//  Copyright © 2018 Universitat de Lleida. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
