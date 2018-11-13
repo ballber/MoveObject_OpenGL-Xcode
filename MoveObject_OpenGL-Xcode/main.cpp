97f0a3083795592578f6deaa9b12f14d1b217872
Josep <josep@mbp13-2.local>
2018-11-13 01:19:26 +0100

    First Commit

--- MoveObject_OpenGL-Xcode/main.cpp
+++ MoveObject_OpenGL-Xcode/main.cpp
@@ -6,10 +6,89 @@
 //  Copyright © 2018 Universitat de Lleida. All rights reserved.
 //
 
-#include <iostream>
+// How to move an object in OpenGL with keyboard arrow key controlling
+// https://faraznaqvi.wordpress.com/2017/08/19/how-to-move-an-object-in-opengl-with-keyboard-arrow-key-controlling/
 
-int main(int argc, const char * argv[]) {
+// First thing first, we have to include the essential files in our project file.
+
+#include <iostream>
+#include <stdlib.h>
+#include <GLUT/GLUT.h>
+
+// including files
+
+using namespace std;
+
+#define WIDTH 600
+#define HEIGHT 400
+
+// Initializing the variables which are going to affect the position of the object.
+float xr = 0, yr = 0; //to control the object’s movement
+
+
+// lets now setup our keyboard controlling function
+void specialkey(int key, int x, int y){
+    switch (key) {
+            //when the up key is pressed
+        case GLUT_KEY_UP:
+            yr =yr + 1;
+            cout<<x<<endl;
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_DOWN: //when the down arrow key is pressed
+            yr--;
+            cout<<x<<endl;
+            glutPostRedisplay();
+            break;
+            //when the left arrow key is pressed
+        case GLUT_KEY_LEFT:
+            xr--;
+            cout<<x<<endl;
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_RIGHT: //when the right arrow key is pressed
+            xr++;
+            cout<<x<<endl;
+            glutPostRedisplay();
+            break;
+    }
+}
+
+//Adding display function now
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1,0, 0);
+    glBegin(GL_QUADS);//object made with the help of quads
+        glVertex2f(20+xr, 10+yr);
+        glVertex2f(30+xr, 10+yr);
+        glVertex2f(30+xr, 20+yr);
+        glVertex2f(20+xr,20+yr);
+    glEnd();
+    glFlush();
+    glutPostRedisplay();
+    glutSwapBuffers();
+}
+
+
+//setting up our main function
+int main(int argc, char **argv){
     // insert code here...
-    std::cout << "Hello, World!\n";
+
+    printf("use array key");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowPosition(600,50);
+    glutCreateWindow("Moving Square");
+    
+    glutDisplayFunc(display);//display call back function
+    
+    glClearColor(0, 0,0,0);
+    gluOrtho2D(0.0,400,0.0,400);
+    glutSpecialFunc(specialkey); // keyboard call back function
+    
+    glutMainLoop();
     return 0;
 }
+
+
